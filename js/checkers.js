edde92c83e2d0b365446f50f07d6ce0c2c25530e
Alexander Berson <aberson@yahoo.com>
2023-07-23 08:35:15 +0200

    moves

--- js/checkers.js
+++ js/checkers.js
@@ -1,37 +1,36 @@
-let board = [[0, 2, 0, 2, 0, 2, 0, 2],
-             [2, 0, 2, 0, 2, 0, 2, 0],
-             [0, 2, 0, 2, 0, 2, 0, 2],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [1, 0, 1, 0, 1, 0, 1, 0],
-             [0, 1, 0, 1, 0, 1, 0, 1],
-             [1, 0, 1, 0, 1, 0, 1, 0]];
+let board;
+let empty = 0;
+let black = 1;
+let white = -1;
+let player = black;
 
 
-const evaluate = (board) => {
+// const evaluate = (board) => {
 
-    let score = 0;
+//     let score = 0;
 
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[i].length; j++) {
-            switch(board[i][j]) {
-                case 1: 
-                    score += 1;
-                    break;
-                case 2:
-                    score -= 1;
-                    break;
-                case 3:
-                    score += 2;
-                    break;
-                case 4:
-                    score -= 2;
-            }
-        }
-    }
+//     for(let i = 0; i < board.length; i++) {
+//         for(let j = 0; j < board[i].length; j++) {
+//             switch(board[i][j]) {
+//                 case 1: 
+//                     score += 1;
+//                     break;
+//                 case 2:
+//                     score -= 1;
+//                     break;
+//                 case 3:
+//                     score += 2;
+//                     break;
+//                 case 4:
+//                     score -= 2;
+//             }
+//         }
+//     }
 
-    return score;
-}
+//     return score;
+// }
+
+const touchScreen = () => matchMedia('(hover: none)').matches;
 
 const setBoardSize = () => {
 
@@ -42,9 +41,121 @@ const setBoardSize = () => {
     document.documentElement.style.setProperty('--board-size', boardSize + 'px');
 }
 
+const initBoard = () => {
+
+    board = [[0, -1, 0, -1, 0, -1, 0, -1],
+             [-1, 0, -1, 0, -1, 0, -1, 0],
+             [0, -1, 0, -1, 0, -1, 0, -1],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 0, 1, 0, 1, 0, 1, 0],
+             [0, 1, 0, 1, 0, 1, 0, 1],
+             [1, 0, 1, 0, 1, 0, 1, 0]];
+}
+
+const fillBoard = () => {
+
+    let pieces = document.querySelectorAll('.piece');
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (board[i][j] == white) pieces[i * 8 + j].classList.add('white');
+            if (board[i][j] == black) pieces[i * 8 + j].classList.add('black');
+        }
+    }
+}
+
+const validMove = (color,r,c,r2,c2) => r2 - r == color * -1 && Math.abs(c2 - c) == 1;
+
+const makeMove = (r,c,r2,c2) => {
+
+    board[r2][c2] = board[r][c];
+    board[r][c] = 0;
+}
+
+const movePiece = (r,c,r2,c2) => {
+
+    let pieces = document.querySelectorAll('.piece');
+    let color = pieces[r * 8 + c].classList.contains('white') ? 'white' : 'black';
+
+    pieces[r * 8 + c].classList.remove(color);
+    pieces[r2 * 8 + c2].classList.add(color);
+}
+
+const select = (e) => {
+
+    let square = e.currentTarget;
+    let squares = document.querySelectorAll('.square');
+    let opponent = player == white ? black : white;
+    let i = [...squares].indexOf(square);
+    let r = Math.trunc(i / 8);
+    let c = i % 8;
+
+    switch (board[r][c]) {
+
+        case opponent:
+            // squares.forEach(square => square.classList.remove('selected'));
+            // return;
+        case player: 
+            squares.forEach(square => square.classList.remove('selected'));
+            square.classList.add('selected');
+            return;
+        case empty :
+
+            let i = [...squares].findIndex(square => square.classList.contains('selected'));
+
+            if (i == -1) return;
+
+            let r0 = Math.trunc(i / 8);
+            let c0 = i % 8;
+
+            if (validMove(board[r0][c0],r0,c0,r,c)) {
+                squares.forEach(square => square.classList.remove('selected'));
+                makeMove(r0,c0,r,c);
+                movePiece(r0,c0,r,c);
+            }
+    }
+}
+
+const enableTouch = () => {
+
+    let squares = document.querySelectorAll('.square');
+
+    for (let square of squares) {
+
+        let event = touchScreen() ? 'touchstart' : 'mousedown';
+
+        square.addEventListener(event, select);
+    }
+}
+
+const disableTouch = () => {
+
+    let squares = document.querySelectorAll('.square');
+
+    for (let square of squares) {
+
+        let event = touchScreen() ? 'touchstart' : 'mousedown';
+
+        square.removeEventListener(event, select);
+    }
+}
+
+const disableTapZoom = () => {
+
+    const preventDefault = (e) => e.preventDefault();
+
+    document.body.addEventListener('touchstart', preventDefault, {passive: false});
+    document.body.addEventListener('mousedown', preventDefault, {passive: false});
+}
+
 const init = () => {
 
+    disableTapZoom();
     setBoardSize();
+    initBoard();
+    fillBoard();
+    enableTouch();
 }
 
 window.onload = () => document.fonts.ready.then(init());
