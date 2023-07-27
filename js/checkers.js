e9897dee34422c6b32cf81a0a4d95980cf08ec68
Alexander Berson <aberson@yahoo.com>
2023-07-27 09:28:30 +0200

    random ai

--- js/checkers.js
+++ js/checkers.js
@@ -5,7 +5,21 @@ let white = -1;
 let player = black;
 let multiJump = false;
 
+const showBoard = () => document.body.style.opacity = 1;
+
 const touchScreen = () => matchMedia('(hover: none)').matches;
+
+const shuffle = (array) => {
+
+    for (let i = array.length - 1; i > 0; i--) {
+
+        let j = Math.trunc(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]]; 
+    }
+
+    return array;
+}
 
 const setBoardSize = () => {
 
@@ -114,15 +128,13 @@ const validMove = (board,r,c,r2,c2) => {
     return r2 - r == color * (-1) && Math.abs(c2 - c) == 1;
 };
 
-const makeMove = (board,r,c,r2,c2) => {
+// const makeMove = (board,r,c,r2,c2) => {
 
-    board[r2][c2] = board[r][c];
-    board[r][c] = 0;
+//     board[r2][c2] = board[r][c];
+//     board[r][c] = 0;
 
-    if ((r2 == 0 || r2 == 7) && Math.abs(board[r2][c2]) == 1) board[r2][c2] *= 2;
-
-    // player = -player;
-}
+//     if ((r2 == 0 || r2 == 7) && Math.abs(board[r2][c2]) == 1) board[r2][c2] *= 2;
+// }
 
 const validJump = (board,r,c,r2,c2) =>  {
 
@@ -135,15 +147,20 @@ const validJump = (board,r,c,r2,c2) =>  {
 }
 
 
-const makeJump = (board,r,c,r2,c2) => {
+const makeMove = (board,r,c,r2,c2) => {
 
+    let king = false;
     board[r2][c2] = board[r][c];
     board[r][c] = 0;
-    board[Math.min(r, r2) + 1][Math.min(c, c2) + 1] = 0
 
-    if ((r2 == 0 || r2 == 7) && Math.abs(board[r2][c2]) == 1) board[r2][c2] *= 2;
+    if (Math.abs(r2 - r) == 2) board[Math.min(r, r2) + 1][Math.min(c, c2) + 1] = 0;
 
-    // player = -player;
+    if ((r2 == 0 || r2 == 7) && Math.abs(board[r2][c2]) == 1) {
+        board[r2][c2] *= 2;
+        king = true;
+    }
+
+    return king;
 }
 
 const win = (board) => {
@@ -156,6 +173,88 @@ const win = (board) => {
 
     return true;
 }
+
+const availableMoves = (board) => {
+
+    const checkMove = (r, c) => c >= 0 && c <= 7 && r >= 0 && r <= 7 && board[r][c] == empty;
+
+    let moves = [];
+
+    for (let r = 0; r < 8; r++) {
+        for (let c = 0; c < 8; c++) {
+
+            let color = Math.sign(board[r][c]);
+
+            if (color != player) continue;
+            if (checkMove(r + color * -1, c - 1)) moves.push([r,c,r + color * -1,c - 1]);
+            if (checkMove(r + color * -1, c + 1)) moves.push([r,c,r + color * -1,c + 1]);
+            if (Math.abs(board[r][c]) != 2) continue;
+            if (checkMove(r + color, c - 1)) moves.push([r,c,r + color,c - 1]);
+            if (checkMove(r + color, c + 1)) moves.push([r,c,r + color,c + 1]);
+        }
+    }
+
+    return moves;
+}
+
+const availableJumps = (board) => {
+
+    const checkJump = (color,r1, c1, r2, c2) => c1 >= 0 && c1 <= 7 && r1 >= 0 && r1 <= 7 && c2 >= 0 && c2 <= 7 && board[r1][c1] == empty && Math.sign(board[r2][c2]) == -color;
+
+    let jumps = [];
+
+    for (let r = 0; r < 8; r++) {
+        for (let c = 0; c < 8; c++) {
+
+            let color = Math.sign(board[r][c]);
+
+            if (color != player) continue;
+            if (checkJump(color,r + color * -2, c - 2, r + color * -1, c - 1)) jumps.push([r,c,r + color * -2,c - 2]);
+            if (checkJump(color,r + color * -2, c + 2, r + color * -1, c + 1)) jumps.push([r,c,r + color * -2,c + 2]);
+            if (Math.abs(board[r][c]) != 2) continue;
+            if (checkJump(color,r + color * 2, c - 2, r + color, c - 1)) jumps.push([r,c,r + color * 2,c - 2]);
+            if (checkJump(color,r + color * 2, c + 2, r + color, c + 1)) jumps.push([r,c,r + color * 2,c + 2]);
+        }
+    }
+
+    return jumps;
+}
+
+const randomAI = (board, r, c) => {
+
+    let jumps = availableJumps(board);
+    let moves = jumps.length > 0 ? jumps : availableMoves(board);
+
+    if (r != -1) {
+
+        shuffle(moves);
+
+        for (let move of moves) {
+            if (move[0] == r, move[1] == c) return move;
+        }
+    }
+
+    return moves[Math.trunc(Math.random() * moves.length)];
+}
+
+const aiMove = (r = -1, c = -1) => {
+
+    let [r1,c1,r2,c2] = randomAI(board, r, c);
+
+    let king = makeMove(board,r1,c1,r2,c2);
+
+    movePiece(r1,c1,r2,c2);
+
+    if (Math.abs(r2 - r1) == 2 && canJump(board,r2,c2) && !king) {
+        setTimeout(aiMove, 500, r2, c2);
+        return;
+    }
+
+    player = -player;
+
+    enableTouch();
+}
+
 
 const movePiece = (r,c,r2,c2) => {
 
@@ -204,14 +303,12 @@ const select = (e) => {
             let r0 = Math.trunc(i / 8);
             let c0 = i % 8;
 
-            console.log(player);
-
             if (validJump(board, r0,c0,r,c)) {
                 squares.forEach(square => square.classList.remove('selected'));
-                makeJump(board,r0,c0,r,c);
+                let king = makeMove(board,r0,c0,r,c);
                 movePiece(r0,c0,r,c);
 
-                if (canJump(board,r,c)) {
+                if (!king && canJump(board,r,c)) {
                     squares[r * 8 + c].classList.add('selected');
                     multiJump = true;
                     return;
@@ -219,6 +316,13 @@ const select = (e) => {
 
                 multiJump = false;
                 player = -player;
+
+                disableTouch();
+                setTimeout(aiMove, 500);
+
+                console.log(availableJumps(board));
+                console.log(availableMoves(board));
+
                 return;
             }
 
@@ -229,6 +333,12 @@ const select = (e) => {
                 makeMove(board,r0,c0,r,c);
                 movePiece(r0,c0,r,c);
                 player = -player;
+
+                disableTouch();
+                setTimeout(aiMove, 500);
+
+                console.log(availableJumps(board));
+                console.log(availableMoves(board));
             }
     }
 }
@@ -272,6 +382,10 @@ const init = () => {
     initBoard();
     fillBoard();
     enableTouch();
+    showBoard();
+
+    console.log(availableJumps(board));
+    console.log(availableMoves(board));
 }
 
 window.onload = () => document.fonts.ready.then(init());
